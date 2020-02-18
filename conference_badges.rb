dd7facea7240a9b04f8896b7e7840807d1a1ac59
Omar B <omar.benjamin377@gmail.com>
2020-02-18 03:39:17 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+speaker = []
+ attendees.each do |attendee| speaker << "Hello, my name is #{attendee}."
+ end
+  return speaker
+ end
+Hash.new
+def assign_rooms
+assigned = []
+  assigns.each { |item| assigned << "Hello, #{item}! You'll be assigned to room #{key}!" }
+assigned
+end
+
+
+speakers = ["Edsger", "Ada", "Charles", "Grace", "Linus", "Matz"]
+puts batch_badge_creator(speakers)
+puts assign_rooms()
